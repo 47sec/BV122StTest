1b94095b0ec6baf67b3fb52bd7c9e5d4fc4000f4
listok195 <72712408+listok195@users.noreply.github.com>
2021-11-12 20:28:46 +0300

    oioli

--- array.cpp
+++ array.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <conio.h>
+void addarr(int** darr, int X, int Y)
+{
+    for (auto i = 0; i < X; ++i)
+    {
+        for (int j = 0; j < Y; ++j)
+            darr[i][j] = 0;
+    }
+}
 
