e4d6d2eb62b1f03fd8e211d9b1d5fb8e11fa0d3e
Ras-Kur <92600019+Ras-Kur@users.noreply.github.com>
2021-11-12 18:37:30 +0300

    изменил на не утф-8

--- interface.cpp
+++ interface.cpp
@@ -34,135 +34,135 @@ int main()
 	}
 	system("cls");
 
-			std::cout << "x1. показать массив \n 2. найти минимум и максимум \n 3. вывести сумму элементов по диагонали (\\) \
+	std::cout << "x1. показать массив \n 2. найти минимум и максимум \n 3. вывести сумму элементов по диагонали (\\) \
             \n 4. вывод среднего арифметического. \n 0. закрыть программу \n";
-			while (_getch==0)
+	while (_getch == 0)
+	{
+
+		switch (_getch())
+		{
+		case 72:
+			if (position >= 1)
 			{
-
-				switch (_getch())
-				{
-				case 72:
-					if (position >= 1)
-					{
-						position--;
-					}
-					else
-					{
-						position = 4;
-					}
-					break;
-				case 80:
-					if (position <= 4)
-					{
-						position++;
-					}
-					else
-						position = 1;
-					break;
-				}
-				//system("cls");
-				switch (position)
-				{
-				case 1:
-					while (_kbhit() == 0)
-					{
-						Sleep(250);
-						std::cout << "-=- 1. показать массив -=-\n";
-						std::cout << "2. найти минимум и максимум\n";
-						std::cout << "3. вывести сумму элементов по диагонали (\\)\n";
-						std::cout << "4. вывод среднего арифметического.\n";
-						std::cout << "0. закрыть программу\n";
-						//system("cls");
-					}
-
-					break;
-
-				case 2:
-					while (_kbhit() == 0)
-					{
-						Sleep(250);
-						std::cout << "1. показать массив \n";
-						std::cout << "2.-=- найти минимум и максимум -=-\n";
-						std::cout << "3. вывести сумму элементов по диагонали (\\)\n";
-						std::cout << "4. вывод среднего арифметического.\n";
-						std::cout << "0. закрыть программу\n";
-						system("cls");
-					}
-					break;
-
-				case 3:
-					while (_kbhit() == 0)
-					{
-						Sleep(250);
-						std::cout << "1. показать массив\n";
-						std::cout << "2. найти минимум и максимум\n";
-						std::cout << "3.-=- вывести сумму элементов по диагонали (\\) -=-\n";
-						std::cout << "4. вывод среднего арифметического.\n";
-						std::cout << "0. закрыть программу\n";
-						system("cls");
-
-					}
-					break;
-
-				case 4:
-					while (_kbhit() == 0)
-					{
-						Sleep(250);
-						std::cout << "1. показать массив\n";
-						std::cout << "2. найти минимум и максимум\n";
-						std::cout << "3. вывести сумму элементов по диагонали (\\)\n";
-						std::cout << "4.-=- вывод среднего арифметического. -=-\n";
-						std::cout << "0. закрыть программу\n";
-						system("cls");
-					}
-				case 0:
-					while (_kbhit() == 0)
-					{
-						Sleep(250);
-						std::cout << "1. показать массив\n";
-						std::cout << "2. найти минимум и максимум\n";
-						std::cout << "3. вывести сумму элементов по диагонали (\\)\n";
-						std::cout << "4. вывод среднего арифметического.\n";
-						std::cout << "-=- 0. закрыть программу -=-\n";
-						system("cls");
-					}
-				default:
-					break;
-				}
-				if (_getch() == 13)
-				{
-					switch (position)
-					{
-					case 1:
-						// вывод масива функция
-						system("pause");
-						break;
-					case 2:
-						//  функция вывода макс и мин
-						system("pause");
-						break;
-					case 3:
-						// сумма по дианонали
-						system("pause");
-						break;
-					case 4:
-						// средне арифмет всего массива
-						
-						system("pause");
-						break;
-					case 0:
-						
-						return 0;
-						system("pause");
-						break;
-					default:
-						std::cout << "ошибка\n";
-						system("pause");
-						break;
-					}
-				}
+				position--;
 			}
-	
-	
+			else
+			{
+				position = 4;
+			}
+			break;
+		case 80:
+			if (position <= 4)
+			{
+				position++;
+			}
+			else
+				position = 1;
+			break;
+		}
+		//system("cls");
+		switch (position)
+		{
+		case 1:
+			while (_kbhit() == 0)
+			{
+				Sleep(250);
+				std::cout << "-=- 1. показать массив -=-\n";
+				std::cout << "2. найти минимум и максимум\n";
+				std::cout << "3. вывести сумму элементов по диагонали (\\)\n";
+				std::cout << "4. вывод среднего арифметического.\n";
+				std::cout << "0. закрыть программу\n";
+				//system("cls");
+			}
+
+			break;
+
+		case 2:
+			while (_kbhit() == 0)
+			{
+				Sleep(250);
+				std::cout << "1. показать массив \n";
+				std::cout << "2.-=- найти минимум и максимум -=-\n";
+				std::cout << "3. вывести сумму элементов по диагонали (\\)\n";
+				std::cout << "4. вывод среднего арифметического.\n";
+				std::cout << "0. закрыть программу\n";
+				system("cls");
+			}
+			break;
+
+		case 3:
+			while (_kbhit() == 0)
+			{
+				Sleep(250);
+				std::cout << "1. показать массив\n";
+				std::cout << "2. найти минимум и максимум\n";
+				std::cout << "3.-=- вывести сумму элементов по диагонали (\\) -=-\n";
+				std::cout << "4. вывод среднего арифметического.\n";
+				std::cout << "0. закрыть программу\n";
+				system("cls");
+
+			}
+			break;
+
+		case 4:
+			while (_kbhit() == 0)
+			{
+				Sleep(250);
+				std::cout << "1. показать массив\n";
+				std::cout << "2. найти минимум и максимум\n";
+				std::cout << "3. вывести сумму элементов по диагонали (\\)\n";
+				std::cout << "4.-=- вывод среднего арифметического. -=-\n";
+				std::cout << "0. закрыть программу\n";
+				system("cls");
+			}
+		case 0:
+			while (_kbhit() == 0)
+			{
+				Sleep(250);
+				std::cout << "1. показать массив\n";
+				std::cout << "2. найти минимум и максимум\n";
+				std::cout << "3. вывести сумму элементов по диагонали (\\)\n";
+				std::cout << "4. вывод среднего арифметического.\n";
+				std::cout << "-=- 0. закрыть программу -=-\n";
+				system("cls");
+			}
+		default:
+			break;
+		}
+		if (_getch() == 13)
+		{
+			switch (position)
+			{
+			case 1:
+				// вывод масива функция
+				system("pause");
+				break;
+			case 2:
+				//  функция вывода макс и мин
+				system("pause");
+				break;
+			case 3:
+				// сумма по дианонали
+				system("pause");
+				break;
+			case 4:
+				// средне арифмет всего массива
+
+				system("pause");
+				break;
+			case 0:
+
+				return 0;
+				system("pause");
+				break;
+			default:
+				std::cout << "ошибка\n";
+				system("pause");
+				break;
+			}
+		}
+	}
+
+
 	return position;
 }
