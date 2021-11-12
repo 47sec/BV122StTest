64968c7b2443d11858a32a659224933b0e2837bf
Ras-Kur <92600019+Ras-Kur@users.noreply.github.com>
2021-11-12 19:36:56 +0300

    gg

--- interface.cpp
+++ interface.cpp
@@ -12,10 +12,10 @@ void setCursorPosition(int x, int y)
 
 int main()
 {
-	SetConsoleCP(866);
-	SetConsoleOutputCP(866);
-	system("chcp 1251");
-	setlocale(LC_ALL, "ru");
+	SetConsoleCP(65001);
+	SetConsoleOutputCP(65001);
+	//system("65001");
+	//setlocale(LC_ALL, "ru");
 	int position = 1;
 	int choose = 1;
 	system("cls");
