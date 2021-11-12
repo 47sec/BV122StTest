82717fbaa9761aa1cfe1a8f0fb54b12d55d76e59
Bois <borchik90@mail.ru>
2021-11-12 21:31:16 +0300

    заполнил масив и вывел его

--- main.cpp
+++ main.cpp
@@ -2,12 +2,32 @@
 #include <fstream>
 
 
-int func(int arr[][], int size_col, int size_str)
+int func(int**arr, int size_col, int size_str)
 {
+	int count = 0;
+	for (int i = 0; i < size_col; i++)
+	{
+		for (int j = 0; j < size_str; j++)
+		{ 
 
+			arr[i][j] = count++;
 
+		}
+	}
 
+	return 1;
+}
+void func_print(int** arr, int size_col, int size_str)
+{
+	for (int i = 0; i < size_col; i++)
+	{
+		for (int j = 0; j < size_str; j++)
+		{
 
+			std::cout << arr[i][j] << '\t';
+			std::cout << '\n';
+		}
+	}
 }
 
 int main()
@@ -19,9 +39,17 @@ int main()
 	// создаем объект класса ifstream
 	file.open("C:\\Users\\BV122\\source\\repos\\BV122StTest\\test_fail.txt"); 
 
-	int coln=0;
-	int arr[n][n];
+	int col_n = 2;
+	int str_n = 2;
+	int** arr;
+	arr = new int*[str_n];
+	for (size_t i = 0; i <= str_n; i++)
+	{
+		arr[i] = new int[col_n];
+	}
+	
 
+	
 	if (!file)
 	{
 		std::cout << "Файл не открыт\n\n";
@@ -33,6 +61,7 @@ int main()
 	
 	}
 
-
+	func(arr,col_n, str_n);
+	func_print(arr, col_n, str_n);
 
 }
